30bd5819818334dc4a6aa1a700d0b7415ec0b44c
Alex Zhao <oatmonster@gmail.com>
2020-04-21 18:39:33 -0700

    Initial Commit

---
+++ egg_bot/__main__.py
@@ -0,0 +1,4 @@
+import transcripts
+
+if __name__ == '__main__':
+    transcripts.main()
---
+++ egg_bot/transcripts.py
@@ -0,0 +1,67 @@
+from __future__ import print_function
+from apiclient import errors
+import pickle
+import re
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/drive.readonly']
+
+def get_files_in_folder(service, page_size, folder_id):
+
+    page_token = None
+
+    results = service.files().list( corpora="allDrives", includeItemsFromAllDrives=True, q="'1F177oBASIbkwv2dSm59sAvGn1YFURIGa' in parents", supportsAllDrives=True, pageSize=page_size).execute()
+
+    items = results.get('files', [])
+    ids = list(map(lambda e: e["id"], items))
+
+    if not ids:
+        print("No files found.")
+    else:
+        print("Files IDs:")
+        for id in ids:
+            print(id)
+    return ids
+
+def get_transcript(service, id):
+
+    # Get the transcript file from the Google Drive API
+    content = service.files().get_media(fileId=id).execute().decode("latin-1")
+
+    # Remove timestamps
+    transcript = " ".join(re.findall("(?:[0-9]{2}\:[0-9]{2})\r\n(.*)\r", content))
+
+    print(transcript)
+    return transcript
+
+def main():
+
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('drive', 'v3', credentials=creds)
+
+    ids = get_files_in_folder(service, 1, "1VJmDJwNZ7mFwcBBPVsioVXtOBs6LMa9X")
+
+    for id in ids:
+        get_transcript(service, id)
